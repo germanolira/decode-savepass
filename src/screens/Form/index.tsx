1e2dea2208af7b62a65f6c287b265e0b4fa1cc11
cnx-germanolira <dolan.3@hotmail.com>
2022-08-21 16:39:01 -0300

    async storage

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,48 +1,76 @@
-import React from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
-import Toast from 'react-native-toast-message';
-import uuid from 'react-native-uuid';
+import React, { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
+import Toast from "react-native-toast-message";
+import uuid from "react-native-uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
-import { HeaderForm } from '../../components/HeaderForm';
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { HeaderForm } from "../../components/HeaderForm";
 
 export function Form() {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
+  async function handleNew() {
+    try {
+      const id = uuid.v4();
+
+      const newData = {
+        id,
+        name,
+        user,
+        password,
+      };
+
+      await AsyncStorage.setItem(
+        "@savepass:passwords",
+        JSON.stringify(newData)
+      );
+      Toast.show({
+        type: "success",
+        position: "top",
+        text1: "Senha salva com sucesso!",
+        visibilityTime: 3000,
+      });
+    } catch (error) {
+      console.log(error);
+      Toast.show({
+        type: "error",
+        position: "top",
+        text1: "Erro ao salvar a senha!",
+        visibilityTime: 3000,
+      });
+    }
+  }
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
       <View style={styles.content}>
         <ScrollView>
-
           <HeaderForm />
 
           <View style={styles.form}>
-            <Input
-              label="Nome do serviço"
-            />
+            <Input label="Nome do serviço" onChangeText={setName} />
             <Input
               label="E-mail ou usuário"
               autoCapitalize="none"
+              onChangeText={setUser}
             />
-            <Input
-              label="Senha"
-              secureTextEntry
-            />
+            <Input label="Senha" secureTextEntry onChangeText={setPassword} />
           </View>
 
           <View style={styles.footer}>
-            <Button
-              title="Salvar"
-            />
+            <Button title="Salvar" onPress={handleNew} />
           </View>
         </ScrollView>
       </View>
-    </KeyboardAvoidingView >
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,47 +1,49 @@
-import { useCallback, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { Card, CardProps } from '../../components/Card';
-import { HeaderHome } from '../../components/HeaderHome';
-import { useFocusEffect } from '@react-navigation/native';
+import { Card, CardProps } from "../../components/Card";
+import { HeaderHome } from "../../components/HeaderHome";
+import { useFocusEffect } from "@react-navigation/native";
 
-import { styles } from './styles';
-import { Button } from '../../components/Button';
+import { styles } from "./styles";
+import { Button } from "../../components/Button";
 
 export function Home() {
   const [data, setData] = useState<CardProps[]>([]);
+
+  async function handleFetchData() {
+    const response = await AsyncStorage.getItem("@savepass:passwords");
+    const data = response ? JSON.parse(response) : {};
+    setData([data]);
+  }
+
+  useEffect(() => {
+    handleFetchData();
+  }, [data]);
 
   return (
     <View style={styles.container}>
       <HeaderHome />
 
       <View style={styles.listHeader}>
-        <Text style={styles.title}>
-          Suas senhas
-        </Text>
+        <Text style={styles.title}>Suas senhas</Text>
 
-        <Text style={styles.listCount}>
-          {`${data.length} ao total`}
-        </Text>
+        <Text style={styles.listCount}>{`${data.length} ao total`}</Text>
       </View>
 
       <FlatList
         data={data}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         style={styles.list}
         contentContainerStyle={styles.listContent}
-        renderItem={({ item }) =>
-          <Card
-            data={item}
-            onPress={() => handleRemove(item.id)}
-          />
-        }
+        renderItem={({ item }) => (
+          <Card data={item} onPress={() => handleRemove(item.id)} />
+        )}
       />
 
       <View style={styles.footer}>
-        <Button
-          title="Limpar lista"
-        />
+        <Button title="Limpar lista" />
       </View>
     </View>
   );
